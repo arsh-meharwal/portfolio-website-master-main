cbc1927d7098f181f2312b38d781e7a74a05e93d
Arsh Meharwal <arshmeharwal98@gmail.com>
2024-08-09 01:03:19 +0530

    Update contact

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,78 +1,5 @@
 "use client";
 
-// import React from "react";
-// import SectionHeading from "./section-heading";
-// import { motion } from "framer-motion";
-// import { useSectionInView } from "@/lib/hooks";
-// import { sendEmail } from "@/actions/sendEmail";
-// import SubmitBtn from "./submit-btn";
-// import toast from "react-hot-toast";
-
-// export default function Contact() {
-//   const { ref } = useSectionInView("Contact");
-
-//   return (
-//     <motion.section
-//       id="contact"
-//       ref={ref}
-//       className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center"
-//       initial={{
-//         opacity: 0,
-//       }}
-//       whileInView={{
-//         opacity: 1,
-//       }}
-//       transition={{
-//         duration: 1,
-//       }}
-//       viewport={{
-//         once: true,
-//       }}
-//     >
-//       <SectionHeading>Contact me</SectionHeading>
-
-//       <p className="text-gray-700 -mt-6 dark:text-white/80">
-//         Please contact me directly at{" "}
-//         <a className="underline" href="mailto:example@gmail.com">
-//           example@gmail.com
-//         </a>{" "}
-//         or through this form.
-//       </p>
-
-//       <form
-//         className="mt-10 flex flex-col dark:text-black"
-//         action={async (formData) => {
-//           const { data, error } = await sendEmail(formData);
-
-//           if (error) {
-//             toast.error(error);
-//             return;
-//           }
-
-//           toast.success("Email sent successfully!");
-//         }}
-//       >
-//         <input
-//           className="h-14 px-4 rounded-lg borderBlack dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
-//           name="senderEmail"
-//           type="email"
-//           required
-//           maxLength={500}
-//           placeholder="Your email"
-//         />
-//         <textarea
-//           className="h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
-//           name="message"
-//           placeholder="Your message"
-//           required
-//           maxLength={5000}
-//         />
-//         <SubmitBtn />
-//       </form>
-//     </motion.section>
-//   );
-// }
-
 
 import { useState } from "react";
 import { useForm, SubmitHandler, FieldValues } from "react-hook-form";
@@ -103,7 +30,7 @@ export default function Contact() {
   const [isSuccess, setIsSuccess] = useState<boolean>(false);
   const [message, setMessage] = useState<string | null>(null);
 
-  const apiKey = `${process.env.API}`;
+  const apiKey = "e80c9181-0005-42b5-8b36-eaf42c31ed6d";
 
   const { submit: onSubmit } = useWeb3Forms<FormData>({
     access_key: apiKey,
